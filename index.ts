643a573a58312a7e3e2ae9461967ca1d42a6989a
CAMBA_Capstone <jgraygo@kent.edu>
2023-03-10 14:32:35 -0500

    Start

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).sendFile(path.join(__dirname, './public/html/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.info(`Listening on port ${PORT}`);
+});
